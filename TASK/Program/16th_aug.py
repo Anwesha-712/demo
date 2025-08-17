0082da78e007b7652ba71514abea624f81f7a2cb
Anwesha-712 <anweshad620@gmail.com>
2025-08-17 20:34:35 +0530

    python task program

---
+++ TASK/Program/16th_aug.py
@@ -0,0 +1,10 @@
+temperature1 = 25
+temperature2 = 40
+
+print("Temperature 1:", temperature1)
+print("Temperature 2:", temperature2)
+
+# Now the divisible by 3 program
+for i in range(1, 101):
+    if i % 3 == 0:
+        print(i, end=", ")
