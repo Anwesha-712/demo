805d4edbb10cd04b0a96127139145a45bbb33505
Anwesha-712 <anweshad620@gmail.com>
2025-08-24 19:40:59 +0530

    New files added for 24th aug

---
+++ TASK/Program/24augLearning.py
@@ -0,0 +1,41 @@
+#min() max()
+#Take the temp reading of the day 10 reading
+#use input/() to take the reading but use a for loop
+# use min/() to find the minimum temp and max to find the maximum 
+# temperature = 12
+# _temperature = 12
+# temperature1 = 12
+# Temperature1 = 12
+# tempe1rature1 = 12
+# tempe_rature1 = 12
+# #1temperature = 12
+# Temp_Mon_Celsius = 10
+# Temp_Mon_Fareh_Bangalore_2025 = 10 
+
+# ctrl + / : comments multiple lines
+
+# temp = {}
+# temperature = 12
+# day = "Monday"
+# bulb = False
+
+# print(f"Type of Temperature is : {type(temperature)} and  Value is : {temperature}")
+# print(type(day))
+# print(type(bulb))
+# print(type(temp))
+
+
+temperature=[] #List data structure
+day_list = ["Mon","Tues","Wed","Thru","Fri"]
+for Day in day_list:
+    for i in range(3):
+        temp =input("enter the temperature of the  " + Day + "day 8hr interval : ")
+        temperature.append(temp)
+
+
+
+
+# for display in range(5):
+#     print(day_list)
+
+
---
+++ TASK/Program/24augLearning_func.py
@@ -0,0 +1,22 @@
+
+def MonToFridayTempReading():
+    temperature=[] #List data structure
+    day_list = ["Mon","Tues","Wed","Thru","Fri"]
+    for Day in day_list:
+        for i in range(3):
+            temp =input("enter the temperature of the  " + Day + "day 8hr interval : ")
+            temperature.append(temp)
+    print(temperature)
+
+ 
+
+# Tak  the temp for mon -friday again and dispay
+
+print("Hello")
+MonToFridayTempReading()
+# find tha average temp value for week 
+
+# logic to find out the average temp for each day 
+
+### line 13 - line 50
+MonToFridayTempReading()
