84f6cbe5eed121b13216bb496f9ee2eb09bf2ec9
Anwesha-712 <anweshad620@gmail.com>
2025-09-17 19:12:22 +0530

    17th sep python class

---
+++ TASK/Program/sep_12_Learning.py
@@ -0,0 +1,14 @@
+import string
+Days_in_Week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+Temp_list = [[15,16,17], [15,33,10], [15,16,17], [33,15,10], [15,16,17], [15,10,33], [20,21,22]]
+
+# print(type(Days_in_Week)) # list 
+# print(type(Days_in_Week[0])) # string 
+
+# # Monday : monday : MONDAY : MonDay 
+# # MONDAY : MONDAY
+# print(Days_in_Week[0].title())
+# print(string.hexdigits)
+# print(type(string.punctuation))
+
+Temp_week_Dict = {"Monday" : [15,16,17], "Tuesday" : 15 , } # 
