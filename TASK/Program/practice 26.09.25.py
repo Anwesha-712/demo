482cc312adfc64853aa4fb8f61aca1538ad87056
Anwesha-712 <anweshad620@gmail.com>
2025-10-05 17:45:55 +0530

    spp 23rd program

---
+++ TASK/Program/practice 26.09.25.py
@@ -0,0 +1,7 @@
+num = 5
+for sv in range (1,num+1):
+    for col in range (sv,0,-1):
+         print(col ,end='')
+    print()
+   
+    
--- TASK/Program/sep_23_learning.py
+++ TASK/Program/sep_23_learning.py
@@ -2,53 +2,53 @@
 temp_1 = {1, 2, 3, 4, 5}
 temp_2 = {4, 5, 6, 7, 8}
 
-print("Set A:", temp_1)
-print("Set B:", temp_2)
+# print("Set A:", temp_1)
+# print("Set B:", temp_2)
 
-# Union
-print("Union:", temp_1 | temp_2)
-print("Union (method):", temp_1.union(temp_2))
+# # Union
+# print("Union:", temp_1 | temp_2)
+# print("Union (method):", temp_1.union(temp_2))
 
-# Intersection
-print("Intersection:", temp_1 & temp_2)
-print("Intersection (method):", temp_1.intersection(temp_2))
+# # Intersection
+# print("Intersection:", temp_1 & temp_2)
+# print("Intersection (method):", temp_1.intersection(temp_2))
 
-# Difference
-print("Difference (A - B):", temp_1 - temp_2)
-print("Difference (method):", temp_1.difference(temp_2))
+# # Difference
+# print("Difference (A - B):", temp_1 - temp_2)
+# print("Difference (method):", temp_1.difference(temp_2))
 
 # Symmetric Difference
-print("Symmetric Difference:", temp_1 ^ temp_2)
-print("Symmetric Difference (method):", temp_1.symmetric_difference(temp_2))
+# print("Symmetric Difference:", temp_2 ^ temp_1)
+# print("Symmetric Difference (method):", temp_1.symmetric_difference(temp_2))
 
-# Subset
-print("A is subset of B:", temp_1 <= temp_2)
-print("A is subset of B (method):", temp_1.issubset(temp_2))
+# # Subset
+# print("A is subset of B:", temp_1 <= temp_2)
+# print("A is subset of B (method):", temp_1.issubset(temp_2))
 
-# Superset
-print("A is superset of B:", temp_1 >= temp_2)
-print("A is superset of B (method):", temp_1.issuperset(temp_2))
+# # Superset
+# print("A is superset of B:", temp_1 >= temp_2)
+# print("A is superset of B (method):", temp_1.issuperset(temp_2))
 
-# Disjoint
-print("A and B are disjoint:", temp_1.isdisjoint(temp_2))
+# # Disjoint
+# print("A and B are disjoint:", temp_1.isdisjoint(temp_2))
 
-# Add element
-temp_1.add(9)
-print("After adding 9 to A:", temp_1)
+# # Add element
+# temp_1.add(9)
+# print("After adding 9 to A:", temp_1)
 
-# Remove element
-temp_1.remove(9)
-print("After removing 9 from A:", temp_1)
+# # Remove element
+# temp_1.remove(9)
+# print("After removing 9 from A:", temp_1)
 
-# Discard element (no error if not present)
-temp_1.discard(10)
-print("After discarding 10 from A (no error):", temp_1)
+# # Discard element (no error if not present)
+# temp_1.discard(10)
+# print("After discarding 10 from A (no error):", temp_1)
 
-# Pop element
-popped = temp_1.pop()
-print("Popped element:", popped)
-print("A after pop:", temp_1)
+# # Pop element
+# popped = temp_1.pop()
+# print("Popped element:", popped)
+# print("A after pop:", temp_1)
 
-# Clear set
-temp_1.clear()
-print("A after clear:", temp_1)
+# # Clear set
+# temp_1.clear()
+# print("A after clear:", temp_1)
